d84b3b285f2521032d7b003d6c244bcb06919a26
Pei-Tang Huang <beta@cht.com.tw>
2016-03-11 16:57:19 +0800

    Add AboutBox, update assembly information.

--- PrintServerLauncher/MainForm.cs
+++ PrintServerLauncher/MainForm.cs
@@ -82,5 +82,10 @@ namespace PrintServerLauncher
         {
 
         }
+
+        private void title_Click(object sender, EventArgs e)
+        {
+            new AboutBox().Show();
+        }
     }
 }
--- PrintServerLauncher/Properties/AssemblyInfo.cs
+++ PrintServerLauncher/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PrintServerLauncher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Print Server Launcher")]
+[assembly: AssemblyDescription("print-server system tray launcher and monitor.\r\n\r\nIcon by ipapun // CC BY")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PrintServerLauncher")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Chhunghwa Telecom Co., Ltd.")]
+[assembly: AssemblyProduct("Print Server Launcher")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
